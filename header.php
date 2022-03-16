97dd8c0fd79099ec56020a324f744c9158596f67
Muhammad Hafiz <leonhaf98@gmail.com>
2022-03-16 17:17:57 +0800

    master

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" contents="Testing website">
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+    <header>
+        <nav id="navigation">
+            <u1>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="https://wwww.makeuseof.com">MakeUseOf</a></li>
+            </u1>
+        </nav>
+    </header>
+    <div id="contents">
+</body>
---
+++ register.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+<head>
+    <title>GFG- Store Data</title>
+</head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<div class="topnav" id="myTopnav">
+  <a href="#home" class="active">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
+<body>
+    
+    <center>
+        <h1>Storing Form data in Database</h1>
+        <form action="insert.php" method="post">    
+<p>
+                <label for="firstName">First Name:</label>
+                <input type="text" name="first_name" id="firstName">
+            </p>           
+<p>
+                <label for="lastName">Last Name:</label>
+                <input type="text" name="last_name" id="lastName">
+            </p>           
+<p>
+                <label for="Gender">Gender:</label>
+                <input type="text" name="gender" id="Gender">
+            </p>
+     
+<p>
+                <label for="Address">Address:</label>
+                <input type="text" name="address" id="Address">
+            </p>
+<p>
+                <label for="emailAddress">Email Address:</label>
+                <input type="text" name="email" id="emailAddress">
+            </p>
+            <input type="submit" value="Submit">
+        </form>
+    </center>
+</body>
+  
+</html>
