bddee38be6182edbbb58d48c5e11b23c9d28f04b
Muhammad Hafiz <leonhaf98@gmail.com>
2022-03-16 19:43:54 +0800

    master

--- register.php
+++ register.php
@@ -7,16 +7,13 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <div class="topnav" id="myTopnav">
-  <a href="#home" class="active">Home</a>
-  <a href="#news">News</a>
-  <a href="#contact">Contact</a>
-  <a href="#about">About</a>
+  <a href="index.php" class="active">Home</a>
   <a href="javascript:void(0);" class="icon" onclick="myFunction()">
     <i class="fa fa-bars"></i>
   </a>
 </div>
+
 <body>
-    
     <center>
         <h1>Storing Form data in Database</h1>
         <form action="insert.php" method="post">    
